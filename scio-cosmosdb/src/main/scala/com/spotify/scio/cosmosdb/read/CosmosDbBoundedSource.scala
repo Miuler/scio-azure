d52f6464c4ff6febd1c6671540d0ed7c8ff7eec3
Hector Miuler Malpica Gallegos <miuler@gmail.com>
2023-02-01 12:07:24 -0500

    📝 docs(scio-cosmosdb): Add Header license, add scaladoc in CosmosDbScioContextOps#readCosmosDbCoreApi and add slf4j-api dependency

--- scio-cosmosdb/src/main/scala/com/spotify/scio/cosmosdb/read/CosmosDbBoundedSource.scala
+++ scio-cosmosdb/src/main/scala/com/spotify/scio/cosmosdb/read/CosmosDbBoundedSource.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2023 Spotify AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.spotify.scio.cosmosdb.read
 
 import com.spotify.scio.annotations.experimental
@@ -10,7 +26,7 @@ import java.util
 import java.util.Collections
 
 /**
- * A CosmosDB Core (SQL) API {@link BoundedSource} reading {@link Document} from a given instance.
+ * A CosmosDB Core (SQL) API [[BoundedSource]] reading [[Document]] from a given instance.
  */
 @experimental
 private[read] class CosmosDbBoundedSource(private[read] val readCosmos: CosmosDbRead)
